7f4472f945c45b632f2ef0d7cb8cb600b0a6f2b1
makeLaugh <wskrdfeai@gmail.com>
2013-08-02 13:22:44 +0800

    Initial Commit

---
+++ ImageEffectDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageEffectDemo
+//
+//  Created by line0 on 13-8-2.
+//  Copyright (c) 2013年 makeLaugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
