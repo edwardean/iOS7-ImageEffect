e1529b6e572020e874dae10bffe7ee1d06427dac
makeLaugh <wskrdfeai@gmail.com>
2013-08-02 14:20:04 +0800

    finished demo

--- ImageEffectDemo/ViewController.m
+++ ImageEffectDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+ImageEffects.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *effectSeg;
+- (IBAction)effectSegmentValueChanged:(UISegmentedControl *)sender;
 
 @end
 
@@ -17,13 +21,65 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self lightEffect];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)lightEffect
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIImage *image = [[UIImage imageNamed:@"Image"] applyLightEffect];
+    [self.imageView setImage:image];
 }
+
+- (void)extraLightEffect
+{
+    UIImage *image = [[UIImage imageNamed:@"Image"] applyExtraLightEffect];
+    [self.imageView setImage:image];
+}
+
+- (void)darkEffect
+{
+    UIImage *image = [[UIImage imageNamed:@"Image"] applyDarkEffect];
+    [self.imageView setImage:image];
+}
+
+- (void)custom1Effect
+{
+    UIImage *image = [[UIImage imageNamed:@"Image"] applyTintEffectWithColor:[[UIColor grayColor] colorWithAlphaComponent:0.2]];
+    [self.imageView setImage:image];
+}
+
+- (void)custom2Effect
+{
+    UIColor *tintColor = [[UIColor greenColor] colorWithAlphaComponent:0.1];
+    UIImage *mastImage = [UIImage imageNamed:@"Image"];
+    UIImage *image = [[UIImage imageNamed:@"bgImage"] applyBlurWithRadius:20 tintColor:tintColor saturationDeltaFactor:1 maskImage:mastImage];
+    [self.imageView setImage:image];
+}
+
+- (IBAction)effectSegmentValueChanged:(UISegmentedControl *)sender
+{
+    switch (sender.selectedSegmentIndex)
+    {
+        case 0:
+            [self lightEffect];
+            break;
+        case 1:
+            [self extraLightEffect];
+            break;
+        case 2:
+            [self darkEffect];
+            break;
+        case 3:
+            [self custom1Effect];
+            break;
+        case 4:
+            [self custom2Effect];
+            break;
+        default:
+            break;
+    }
+}
+
 
 @end
